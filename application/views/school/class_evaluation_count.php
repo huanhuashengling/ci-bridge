e96eda1a72f5064b31e70f8638943a78a935f1d2
ywj <shengling_2005@163.com>
2017-05-17 06:41:20 +0800

    remove no need div tag

--- application/views/school/class_evaluation_count.php
+++ application/views/school/class_evaluation_count.php
@@ -1,32 +1,24 @@
 <div class="container">
-    <div class="panel panel-default">
-            <div class="panel-body">
-                <div class='col-md-12'>
-                    <div class='col-md-12'><h4>请选课程</h4></div>
-                    <div class='col-md-12'>
-                        <div class="btn-group" name="course-btn-group" data-toggle="buttons">
-                            <label class='btn btn-default course-btn btn-lg active'><input type='radio'>全部</label>
-                        <?php
-                            foreach ($courses as $key => $course) {
-                                echo "<label class='btn btn-default btn-lg course-btn'><input type='radio' id='" . $course['id'] . "'>" . $course['name'] . "</label>";
-                            }
-                        ?>
-                        </div>
-                    </div>
+    <div class="panel panel-success">
+            <div class="panel-heading">
+                <h4>请选课程</h4>
+                <div class="btn-group" name="course-btn-group" data-toggle="buttons">
+                    <label class='btn btn-primary course-btn btn-lg active'><input type='radio'>全部</label>
+                <?php
+                    foreach ($courses as $key => $course) {
+                        echo "<label class='btn btn-primary btn-lg course-btn'><input type='radio' id='" . $course['id'] . "'>" . $course['name'] . "</label>";
+                    }
+                ?>
                 </div>
-                <div class='col-md-12'>
-                    <div class='col-md-11'><h4>请选班级</h4></div>
-                    <div class='col-md-12'>
-                        <div class='btn-group' name='class-btn-group' data-toggle='buttons'>
-                        <?php
-                            foreach ($classesData as $key => $gradeClasses) {
-                                foreach ($gradeClasses as $key => $class) {
-                                    echo "<label class='btn btn-default btn-lg class-btn'><input type='radio' id='" . $class['id'] . "'>" . $class['name'] . "</label>";
-                                }
-                            }
-                        ?>
-                        </div>
-                    </div>
+                <h4>请选班级</h4>
+                <div class='btn-group' name='class-btn-group' data-toggle='buttons'>
+                <?php
+                    foreach ($classesData as $key => $gradeClasses) {
+                        foreach ($gradeClasses as $key => $class) {
+                            echo "<label class='btn btn-primary btn-lg class-btn'><input type='radio' id='" . $class['id'] . "'>" . $class['name'] . "</label>";
+                        }
+                    }
+                ?>
                 </div>
             </div>
         </div>
